72464677f1ed4b24c32165c9d09d6586ba02660d
Андрей <Xaritonov.A.A@yandex.ru>
2025-07-17 04:26:34 +0300

    Add files via upload

---
+++ Stack/MinStack.java
@@ -0,0 +1,45 @@
+package com.problem_solving.Needcode150.Stack;
+
+public class MinStack {
+
+    public static void main(String[] args) {
+
+    }
+    private Node head;
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val, null);
+        } else {
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private static class Node {
+        int val;
+        int min;
+        Node next;
+
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
