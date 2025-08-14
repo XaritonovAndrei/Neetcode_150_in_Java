493ae2b7954fd6aa086b803a15020d9e647288d2
Андрей <Xaritonov.A.A@yandex.ru>
2025-08-14 20:14:08 +0300

    Add files via upload

---
+++ Backtracking/PalindromePartitioning.java
@@ -0,0 +1,49 @@
+package com.problem_solving.Needcode150.Backtracking;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ Given a string s,
+ partition s such that every substring of the partition is a palindrome.
+ Return all possible palindrome partitioning of s.
+ */
+
+public class PalindromePartitioning {
+
+    public static void main(String[] args) {
+        String s = "aab";
+        System.out.println(partition(s));
+    }
+
+    public static List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    private static void backtrack(List<List<String>> result, List<String> current, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(result, current, s, end + 1);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private static boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
