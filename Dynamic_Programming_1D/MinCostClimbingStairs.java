8ad0e55fce5ea4cf37de6bef24ff5c007d5b87b8
Андрей <Xaritonov.A.A@yandex.ru>
2025-08-16 20:22:08 +0300

    Add files via upload

---
+++ Dynamic_Programming_1D/MinCostClimbingStairs.java
@@ -0,0 +1,28 @@
+package com.problem_solving.Needcode150.Dynamic_Programming_1D;
+
+import java.util.Arrays;
+
+class MinCostClimbingStairs {
+
+    public static void main(String[] args) {
+        int[] cost = {10,15,20};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 1) return cost[0];
+        if (cost.length == 2) return Math.min(cost[0], cost[1]);
+
+        int first = cost[0];
+        int second = cost[1];
+
+        for (int i = 2; i < cost.length; i++) {
+            int current = Math.min(first, second) + cost[i];
+            first = second;
+            second = current;
+        }
+
+        // Minimum cost to reach the last step or one step past it
+        return Math.min(first, second);
+    }
+}
