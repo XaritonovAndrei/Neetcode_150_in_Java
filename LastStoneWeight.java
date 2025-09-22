77dd4a589cf371f8d59861f58464a10822a435a6
Андрей <Xaritonov.A.A@yandex.ru>
2025-09-22 18:28:17 +0300

    Add files via upload

---
+++ LastStoneWeight.java
@@ -0,0 +1,25 @@
+package com.problem_solving.Needcode150;
+
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+
+            if (x != y) {
+                maxHeap.add(y - x);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
